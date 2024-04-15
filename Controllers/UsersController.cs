45331844aaedb31f6dc24eed63c2f7d6d78d64e9
YuvalZin <yuval.zindani@gmail.com>
2024-04-15 16:40:15 +0300

    The original woofr server

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using woofr.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace woofr.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public int Post([FromBody] User u)
+        {
+            return u.RegisterUser();
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("UploadImage/{id}")]
+        public bool UploadImage(int id, [FromBody] string image)
+        {
+            User u = new();
+            return u.UploadImage(id, image);
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,48 @@
+﻿namespace woofr.Models
+{
+    public class User
+    {
+        private int userId; 
+        private string username;
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string password; 
+        private string gender;
+        //private string profilePictureUrl; 
+        //private string bio; 
+        private DateTime birthday;
+
+        public int UserId { get => userId;}
+        public string Username { get => username; set => username = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public string Gender { get => gender; set => gender = value; }
+        //public string ProfilePictureUrl { get => profilePictureUrl; set => profilePictureUrl = value; }
+        //public string Bio { get => bio; set => bio = value; }
+        public DateTime Birthday { get => birthday; set => birthday = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+
+        public int RegisterUser()
+        {
+            DBservices dbs = new DBservices();
+            int u_id = dbs.RegisterUser(this);
+            if (u_id > 0) return u_id;
+            return -1;
+        }
+        public bool UploadImage(int id, string image)
+        {
+            DBservices dbs = new DBservices();
+            int rowsAff = dbs.UploadImage(id,image);
+            if (rowsAff ==1 ) return true;
+            return false;
+        }
+
+        
+
+        //public DateTime RegistrationDate { get; set; }
+        //public DateTime LastLoginDate { get; set; }
+        //public AccountStatus Status { get; set; } // Enum: Active, Suspended, Deleted
+    }
+}

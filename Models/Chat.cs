85bae007c3eb0534f9f981424de27d51b64df70c
nevoavisar97 <119767217+nevoavisar97@users.noreply.github.com>
2024-04-25 01:24:14 +0300

    chat and message class

---
+++ Models/Chat.cs
@@ -0,0 +1,19 @@
+﻿namespace woofr.Models
+{
+    public class Chat
+    {
+        private string chatID;
+        private string participant1ID;
+        private string participant2ID;
+        private int participant1UnreadCount;
+        private int participant2UnreadCount;
+        private string lastMessage;
+
+        public string ChatID { get => chatID; set => chatID = value; }
+        public string Participant1ID { get => participant1ID; set => participant1ID = value; }
+        public string Participant2ID { get => participant2ID; set => participant2ID = value; }
+        public int Participant1UnreadCount { get => participant1UnreadCount; set => participant1UnreadCount = value; }
+        public int Participant2UnreadCount { get => participant2UnreadCount; set => participant2UnreadCount = value; }
+        public string LastMessage { get => lastMessage; set => lastMessage = value; }
+    }
+}
